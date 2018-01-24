2bd24e18cb5cf03a18f888e7a74bdb4b59dbc234
elenasheloputova <33489810+elenasheloputova@users.noreply.github.com>
2018-01-24 17:04:39 +0300

    Первые тесты VTBDBOTLAB-5404

--- src/test/java/test/first/CheckPairOfAccountBic.java
+++ src/test/java/test/first/CheckPairOfAccountBic.java
@@ -1,4 +1,108 @@
 package test.first;
 
-public class CheckPairOfAccountBic {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import utils.ConfigProperties;
+
+public class CheckPairOfAccountBic extends TestBase {
+
+
+  @Test
+
+  public void clearAccountField() {
+
+    String noaccont = "Не указан счет получателя, что допустимо только в случае, если получатель - кредитная организация.";
+
+    goToMyPP();
+
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).click();
+    driver.findElement(By.xpath("//div[@class='field__clean']/i")).click();
+    driver.findElement(By.xpath("//div[@class='Button__labelClass--_0Dmv' and text()='Сохранить']")).click();
+
+    WebElement warningAccount = driver.findElement(By.xpath("//div[@class=\"message\"]//div[@class='message__text']"));
+    wait.until(ExpectedConditions.visibilityOf(warningAccount));
+    warningAccount.click();
+    warningAccount.getText();
+    Assert.assertTrue("Сообщения при сохранении ПП без счета не совпадают", warningAccount.getText().equals(noaccont));
+
+  }
+
+  @Test
+  public void successfulSave() {
+
+    String sucSaved = "Документ успешно сохранен";
+
+    goToMyPP();
+
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).click();
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).clear();
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).sendKeys(ConfigProperties.getTestProperty("account1"));
+    driver.findElement(By.xpath("//div[@title='БИК']/div[2]/div[1]/input")).click();
+    driver.findElement(By.xpath("//div[@title='БИК']/div[2]/div[1]/input")).clear();
+    driver.findElement(By.xpath("//div[@title='БИК']/div[2]/div[1]/input")).sendKeys(ConfigProperties.getTestProperty("bic1"));
+    driver.findElement(By.xpath("//div[@class='Button__labelClass--_0Dmv' and text()='Сохранить']")).click();
+
+    WebElement successfulSaved = driver.findElement(By.xpath("//div[@class='PushItem__text--14a_h' and text()='Документ успешно сохранен']"));
+    wait.until(ExpectedConditions.visibilityOf(successfulSaved));
+    successfulSaved.click();
+    successfulSaved.getText();
+    Assert.assertTrue("Сообщения не совпадают", successfulSaved.getText().equals(sucSaved));
+
+  }
+
+
+  @Test
+  public void incorrectPairsAccountBic() {
+
+    String messageForAccount = "Ключ счета получателя неверен. Должен быть 1";
+
+    goToMyPP();
+
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).click();
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).clear();
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).sendKeys(ConfigProperties.getTestProperty("account2"));
+    driver.findElement(By.xpath("//div[@class='Button__labelClass--_0Dmv' and text()='Сохранить']")).click();
+
+    WebElement messageAccount = driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']//div[@class='field__tooltip field__tooltip_error']"));
+    wait.until(ExpectedConditions.visibilityOf(messageAccount));
+    messageAccount.click();
+    messageAccount.getText();
+    Assert.assertTrue("Сообщения при сохранении ПП без счета не совпадают", messageAccount.getText().equals(messageForAccount));
+
+  }
+
+  @Test
+  public void enterAccountMorePossible() {
+    String size = "11111.810.8.11111111111";
+
+    goToMyPP();
+
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).click();
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).clear();
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).sendKeys(ConfigProperties.getTestProperty("account3"));
+//    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).sendKeys(ConfigProperties.getTestProperty("account2"));
+//    driver.findElement(By.xpath("//div[@class='Button__labelClass--_0Dmv' and text()='Сохранить']")).click();
+
+    WebElement account21 = driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input"));
+    //wait.until(ExpectedConditions.visibilityOf(messageAccount));
+    //messageAccount.click();
+    String text = account21.getText();
+    Assert.assertTrue("Счета не совпадают", text.length() == 23);
+
+
+  }
+
 }
+
+
+
+
+
+
+
+
+
--- src/test/java/test/first/TestBase.java
+++ src/test/java/test/first/TestBase.java
@@ -1,4 +1,92 @@
-package Test.first;
+package test.first;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.ConfigProperties;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 public class TestBase {
+  final static Logger logger = Logger.getLogger(BanksLetters.class);
+  public WebDriver driver;
+  public WebDriverWait wait;
+  public Actions action;
+  public DesiredCapabilities capabilities;
+  /*public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+
+
+  public boolean isElementPresent(final By locator) {
+    try {
+      wait.until((WebDriver d) -> d.findElement(locator));
+      return true;
+    } catch (TimeoutException ex) {
+      return false;
+    }
+  }*/
+
+  @Before
+  public void start() {
+
+    capabilities = new DesiredCapabilities();
+    driver = new ChromeDriver(capabilities);
+
+   /* ChromeOptions options = new ChromeOptions();
+    options.addArguments("start-fullscreen"); // добавили аргумент start-fullscreen
+    driver = new ChromeDriver(options);*/
+
+
+    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); // неявные ожидания
+    wait = new WebDriverWait(driver, 10);
+    login("login1", "password1");
+
+  }
+
+  private void login(String login, String password) {
+    login = ConfigProperties.getTestProperty("login1");
+    password = ConfigProperties.getTestProperty("password1");
+    driver.get(ConfigProperties.getTestProperty("url"));
+
+    driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+
+    driver.findElement(By.xpath("//input[@type='text']")).click();
+    driver.findElement(By.xpath("//input[@type='text']")).clear();
+    driver.findElement(By.xpath("//input[@type='text']")).sendKeys(login);
+    driver.findElement(By.xpath("//input[@type='password']")).click();
+    driver.findElement(By.xpath("//input[@type='password']")).clear();
+    driver.findElement(By.xpath("//input[@type='password']")).sendKeys(password);
+    driver.findElement(By.cssSelector(".LoginView__btnPrimary--3LQGe[type='submit']")).click();
+  }
+
+  public void goToMyPP() {
+    driver.findElement(By.linkText("Расчеты в рублях")).click();
+    wait.until(visibilityOfElementLocated(By.linkText("Расчеты в рублях")));
+    driver.findElement(By.linkText("Расчеты в рублях")).click();
+
+    WebElement mypp = driver.findElement(By.xpath("//div[contains(@class,'table__row')]//div[text()=\"Elena\"]"));
+    wait.until(ExpectedConditions.elementToBeClickable(mypp));
+    mypp.click();
+    mypp = driver.findElement(By.xpath("//div[contains(@class,'table__row')]//div[text()=\"Elena\"]"));
+
+    action = new Actions(driver);
+    action.doubleClick(mypp).build().perform();
+  }
+
+  @After
+  public void stop() {
+    driver.quit();
+    driver = null;
+  }
 }
