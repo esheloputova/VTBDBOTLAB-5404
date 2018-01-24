299f5955152ace786fae792cf1562a354e984960
elenasheloputova <33489810+elenasheloputova@users.noreply.github.com>
2018-01-24 20:05:55 +0300

    Все проверки по задаче VTBDBOTLAB-5404

--- src/test/java/test/first/CheckPairOfAccountBic.java
+++ src/test/java/test/first/CheckPairOfAccountBic.java
@@ -15,7 +15,7 @@ public class CheckPairOfAccountBic extends TestBase {
 
   public void clearAccountField() {
 
-    String noaccont = "Не указан счет получателя, что допустимо только в случае, если получатель - кредитная организация.";
+    String noaccount = "Не указан счет получателя, что допустимо только в случае, если получатель - кредитная организация.";
 
     goToMyPP();
 
@@ -27,7 +27,7 @@ public class CheckPairOfAccountBic extends TestBase {
     wait.until(ExpectedConditions.visibilityOf(warningAccount));
     warningAccount.click();
     warningAccount.getText();
-    Assert.assertTrue("Сообщения при сохранении ПП без счета не совпадают", warningAccount.getText().equals(noaccont));
+    Assert.assertTrue("Сообщения при сохранении ПП без счета не совпадают", warningAccount.getText().equals(noaccount));
 
   }
 
@@ -77,26 +77,63 @@ public class CheckPairOfAccountBic extends TestBase {
 
   @Test
   public void enterAccountMorePossible() {
-    String size = "11111.810.8.11111111111";
 
     goToMyPP();
 
     driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).click();
     driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).clear();
     driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).sendKeys(ConfigProperties.getTestProperty("account3"));
-//    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).sendKeys(ConfigProperties.getTestProperty("account2"));
-//    driver.findElement(By.xpath("//div[@class='Button__labelClass--_0Dmv' and text()='Сохранить']")).click();
 
     WebElement account21 = driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input"));
-    //wait.until(ExpectedConditions.visibilityOf(messageAccount));
-    //messageAccount.click();
-    String text = account21.getText();
-    Assert.assertTrue("Счета не совпадают", text.length() == 23);
+    String fieldAccount = account21.getAttribute("defaultValue");
+    Assert.assertTrue("Длина счета не 20 символов", fieldAccount.length() == 23);
+  }
 
+  @Test
+  public void enterAccountLesPossible() {
+
+    String messageAboutLengthAccount = "Длина значения поля счет получателя должна быть 20 символов";
+
+    goToMyPP();
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).click();
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).clear();
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).sendKeys(ConfigProperties.getTestProperty("account4"));
+    driver.findElement(By.xpath("//div[@class='Button__labelClass--_0Dmv' and text()='Сохранить']")).click();
+
+    WebElement account19 = driver.findElement(By.xpath("//div[@class=\"field__tooltip field__tooltip_error\"]"));
+    String textError = account19.getText();
+    Assert.assertTrue("Длина счета меньше 20 символов", textError.equals(messageAboutLengthAccount));
 
   }
 
+  @Test
+  public void enterLettersInAccount() {
+    goToMyPP();
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).click();
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).clear();
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).sendKeys(ConfigProperties.getTestProperty("account5"));
+
+    WebElement account0 = driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input"));
+    String fieldAccountLetters = account0.getAttribute("defaultValue");
+    Assert.assertTrue("В поле счет можно ввести буквы", fieldAccountLetters.length() == 0);
+
+  }
+
+  @Test
+  public void enterCharactersInAccount() {
+    goToMyPP();
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).click();
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).clear();
+    driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input")).sendKeys(ConfigProperties.getTestProperty("account6"));
+
+    WebElement accountCharacters = driver.findElement(By.xpath("//div[@title='Расчетный счет получателя']/div[2]/input"));
+    String fieldAccountChar = accountCharacters.getAttribute("defaultValue");
+    Assert.assertTrue("В поле счет можно ввести спецсимволы", fieldAccountChar.length() == 0);
+
+  }
 }
+
+
 
 
 
